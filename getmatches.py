1c1d6bfe8d81ec7643ae7bc561d5bbc065155e78
Ferdinand Hubbard <ferdinandhubbard@gmail.com>
2020-08-30 17:29:37 +0200

    functional

--- getmatches.py
+++ getmatches.py
@@ -2,14 +2,19 @@ import requests
 import time
 
 def sendrequest(url):
+    apikeys = ["RGAPI-9fdbc9a6-1a65-4b82-bfe0-fe5bfcc57370", "RGAPI-94872c2a-cb5e-4f46-bec1-74a9da9e45e9"]
+    index = 0
     successful = False
     response = []
     while successful == False:
-        response = requests.get(url).json()
+        response = requests.get(url + apikeys[index]).json()
         try:
             if response["status"]["status_code"] == 429:
                 print("\nrate limit exceeded\n")
-                time.sleep(10)
+                index += 1
+                if index == len(apikeys):
+                    index = 0
+                time.sleep(0.5)
 
             else:
                 print("\nunknown error, status code: " + str(response["status"]["status_code"]) + "\n")
@@ -18,8 +23,8 @@ def sendrequest(url):
         except KeyError as e:
             successful = True
     return response
-def getmatchhistoryids(apikey, encryptedaccountid):
-    url = "https://euw1.api.riotgames.com/lol/match/v4/matchlists/by-account/" + encryptedaccountid + "?api_key=" + apikey
+def getmatchhistoryids(encryptedaccountid):
+    url = "https://euw1.api.riotgames.com/lol/match/v4/matchlists/by-account/" + encryptedaccountid + "?api_key="
     response = sendrequest(url)
     #print(response["totalGames"])
     gameids = []
@@ -28,52 +33,104 @@ def getmatchhistoryids(apikey, encryptedaccountid):
             gameids.append(game["gameId"])
     return gameids
 
-def getencryptedaccountid(summonername, apikey):
-    url = "https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + summonername + "?api_key=" + apikey
+def writeoldgameids(gameids):
+    with open("oldgameids.txt", "a") as f:
+        for gameid in gameids:
+            f.write(str(gameid) + "\n")
+
+def getencryptedaccountid(summonername):
+    url = "https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + summonername + "?api_key="
     response = sendrequest(url)
     return response["accountId"]
 
-def getchamps(gameid, apikey):
-        url = "https://euw1.api.riotgames.com/lol/match/v4/matches/" + str(gameid) + "?api_key=" + apikey
+def getchamps(gameid):
+        url = "https://euw1.api.riotgames.com/lol/match/v4/matches/" + str(gameid) + "?api_key="
         response = sendrequest(url)
         champions = []
         for player in response["participants"]:
             champions.append(player["championId"])
         return champions
 
-def writecompositiontofile(champions, gameid):
-    with open(str(gameid) + ".txt", "w") as f:
+def writechampionstofile(champions, gameid):
+    with open("games/" + str(gameid) + ".txt", "w") as f:
         for champion in champions:
-            f.writeline(champion) # NOT TESTED YET
+            f.write(str(champion) + "\n") # NOT TESTED YET
 
 
-def getplayers(apikey, gameid):
-    url = "https://euw1.api.riotgames.com/lol/match/v4/matches/" + str(gameid) + "?api_key=" + apikey
+def getplayers(gameid):
+    url = "https://euw1.api.riotgames.com/lol/match/v4/matches/" + str(gameid) + "?api_key="
     response = sendrequest(url)
     accountids = []
     for participant in response["participantIdentities"]:
         accountids.append(participant["player"]["accountId"])
     return accountids
+
+def writenewaccountids(players):
+    with open("newaccountids.txt", "a") as f:
+        for player in players:
+            f.write(player + "\n")
+
+
+def writeoldaccountid(id):
+    with open("oldaccountids.txt", "a") as f:
+        f.write(id + "\n")
+
+
 def deleterepeatedgameids(gameids):
     repeatedgames = []
-    with open("previousgameids.txt", "r") as f:
-        previousgameids = f.read()
-        previousgameids = previousgameids.splitlines()
+    with open("oldgameids.txt", "r") as f:
+        previousgameids = f.read().splitlines() #problem maybe
         for oldid in previousgameids:
-            for gameid in gameids:
+            for a in gameids:
                 if str(gameid) == oldid:
                     gameids.remove(gameid)
     return gameids
+def getnewaccountids(amount):
+    chosenaccountids = []
+    allaccountids = []
+    with open("newaccountids.txt", "r") as f:
+        allaccountids = f.read().splitlines()
+        if amount > len(allaccountids):
+            amount = len(allaccountids)
+        chosenaccountids = allaccountids[:amount]
+
+        allaccountids = allaccountids[amount:]
+    #to remove ids that i have just read and put them in temp file in case of misclicks
+    with open("newaccountids.txt", "w") as f:
+        for id in allaccountids:
+            f.write(id + "\n")
+    with open("tempdeletedaccountids.txt", "w") as f:
+        for id in chosenaccountids:
+            f.write(id + "\n")
+    return chosenaccountids
+
+def cycle(accountid):
+    gameids = getmatchhistoryids(accountid)
+    gameids = deleterepeatedgameids(gameids)
+    #gameids = [gameids[0]] #TEMPORARY
+    players = []
+    for gameid in gameids:
+        newplayers = getplayers(gameid)
+        newplayers.remove(accountid)
+        for player in newplayers: #merging the newplayers list to the full players list
+            players.append(player)
+
+        champions = getchamps(gameid)
+        writechampionstofile(champions, gameid)
+        print("done")
+
+    writeoldgameids(gameids)
+    writenewaccountids(players)
+    writeoldaccountid(accountid)
+
 
 def main():
-    apikey = "RGAPI-d9ce9e7f-87cb-49b4-8684-9017e6c7b831"
-    summonername = "hubbix"
-    accountid = getencryptedaccountid(summonername, apikey)
-    gameids = getmatchhistoryids(apikey, accountid)
-    print(len(gameids))
-    gameids = deleterepeatedgameids(gameids)
-    print(len(gameids))
-    #players = getplayers(apikey, gameids[0])
-    #champions = getchamps(gameids[0], apikey)
+    numofaccountids = 2
+    #summonername = "hubbix" #if new accountids is empty
+    #accountids = [getencryptedaccountid(summonername)] #if new accountids is empty
+    accountids = getnewaccountids(numofaccountids)
+    for accountid in accountids:
+        cycle(accountid)
+
 if __name__ == "__main__":
     main()
